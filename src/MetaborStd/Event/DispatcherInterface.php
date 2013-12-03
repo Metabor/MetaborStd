9ca53fac5d9a6266182011d7ece1ac3523dbe136
Oliver Tischlinger <oliver.tischlinger@project-a.com>
2013-12-03 18:26:38 +0100

    add an changeable event dispatcher

---
+++ src/MetaborStd/Event/DispatcherInterface.php
@@ -0,0 +1,25 @@
+<?php
+namespace MetaborStd\Event;
+
+use MetaborStd\CallbackInterface;
+
+/**
+ *
+ * @author Oliver Tischlinger
+ *        
+ */
+interface DispatcherInterface extends CallbackInterface
+{
+    /**
+     *
+     * @param EventInterface $event
+     * @param array $arguments
+     * @param callable $onReadyCallback
+     */
+    public function dispatch(EventInterface $event, array $arguments = array(), CallbackInterface $onReadyCallback = null);
+
+    /**
+     * @return boolean
+     */
+    public function isReady();
+}
