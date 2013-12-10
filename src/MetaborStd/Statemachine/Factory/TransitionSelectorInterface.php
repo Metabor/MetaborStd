34557afba485d1b28f3abb99b6abadfd42eb06f5
Oliver Tischlinger <oliver.tischlinger@project-a.com>
2013-12-10 15:50:38 +0100

    add selector for active transiton to be used

---
+++ src/MetaborStd/Statemachine/Factory/TransitionSelectorInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace MetaborStd\Statemachine\Factory;
+/**
+ * @author otischlinger
+ *
+ */
+interface TransitionSelectorInterface
+{
+    /**
+     * @param \Traversable $transitions
+     * @return \MetaborStd\Statemachine\TransitionInterface | null
+     */
+    public function selectTransition(\Traversable $transitions);
+}
