27cf74da2e6448e9df2cf5fce731cb859acafe28
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-05-27 22:39:33 +0200

    add time and money type

---
+++ src/MetaborStd/MonetaryInterface.php
@@ -0,0 +1,24 @@
+<?php
+namespace MetaborStd;
+
+/**
+ *
+ * @author Oliver Tischlinger
+ *        
+ */
+interface MonetaryInterface
+{
+
+    /**
+     *
+     * @return string
+     */
+    public function getCurrency();
+
+    /**
+     *
+     * @return number
+     */
+    public function getAmount();
+
+}
---
+++ src/MetaborStd/TimeBasedInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace MetaborStd;
+
+/**
+ *
+ * @author Oliver Tischlinger
+ *        
+ */
+interface TimeBasedInterface
+{
+
+    /**
+     *
+     * @return \DateTime
+     */
+    public function getDate();
+}
