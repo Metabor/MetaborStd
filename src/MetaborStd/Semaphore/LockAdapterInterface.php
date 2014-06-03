a9c41d43e3ad5f198e6392e995ab0bdaf1d51a2a
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-06-03 14:20:23 +0200

    adding semaphore functionality

---
+++ src/MetaborStd/Semaphore/LockAdapterInterface.php
@@ -0,0 +1,27 @@
+<?php
+namespace MetaborStd\Semaphore;
+/**
+ * @author Oliver Tischlinger
+ *
+ */
+interface LockAdapterInterface
+{
+
+    /**
+     * @param string $resourceName
+     * @return bool
+     */
+    public function acquireLock($resourceName);
+
+    /**
+     * @param $resourceName
+     * @return bool
+     */
+    public function releaseLock($resourceName);
+
+    /**
+     * @param $resourceName
+     * @return bool
+     */
+    public function isLocked($resourceName);
+}
---
+++ src/MetaborStd/Semaphore/MutexInterface.php
@@ -0,0 +1,29 @@
+<?php
+namespace MetaborStd\Semaphore;
+/**
+ * @author Oliver Tischlinger
+ *
+ */
+interface MutexInterface
+{
+
+    /**
+     * @return bool
+     */
+    public function acquireLock();
+
+    /**
+     * @return bool
+     */
+    public function releaseLock();
+
+    /**
+     * @return bool
+     */
+    public function isLocked();
+
+    /**
+     * @return bool
+     */
+    public function isAcquired();
+}
