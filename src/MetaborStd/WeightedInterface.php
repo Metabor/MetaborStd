d2320800f58237e41cb02bd0c2949324c472525f
Oliver Tischlinger <oliver.tischlinger@kyto.com>
2016-10-04 14:00:21 +0200

    add WeightedInterface

---
+++ src/MetaborStd/WeightedInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MetaborStd;
+
+/**
+ * @author Oliver Tischlinger
+ */
+interface WeightedInterface
+{
+    /**
+     * @return double
+     */
+    public function getWeight();
+}
